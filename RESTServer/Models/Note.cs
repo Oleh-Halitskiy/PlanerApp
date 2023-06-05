0c5bde4f8947d81861ae97bf093282455ae00fc3
Ex0oleg <olegexo@gmail.com>
2023-06-05 23:59:50 +0300

    added models

---
+++ RESTServer/Models/Note.cs
@@ -0,0 +1,33 @@
+﻿namespace RESTServer.Models
+{
+    public class Note
+    {
+        private int id;
+        private string title;
+        private string time;
+        private int userID;
+        private DateTime noteDate;
+        private bool isChecked;
+
+        public int ID { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Time { get => time; set => time = value; }
+        public int UserID { get => userID; set => userID = value; }
+        public DateTime NoteDate { get => noteDate; set => noteDate = value; }
+        public bool IsChecked { get => isChecked; set => isChecked = value; }
+
+        public Note()
+        {
+
+        }
+
+        public Note(string title, string time, int userID, DateTime noteDate, bool isChecked)
+        {
+            Title = title;
+            Time = time;
+            UserID = userID;
+            NoteDate = noteDate;
+            IsChecked = isChecked;
+        }
+    }
+}
---
+++ RESTServer/Models/User.cs
@@ -0,0 +1,32 @@
+﻿namespace RESTServer.Models
+{
+    public class User
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private string login;
+        private string password;
+        private string email;
+
+        public int ID { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Login { get => login; set => login = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+
+        public User()
+        {
+
+        }
+        public User(string firstName, string lastName, string login, string password, string email)
+        { 
+            FirstName = firstName;
+            LastName = lastName;
+            Login = login;
+            Password = password;
+            Email = email;
+        }
+    }
+}
