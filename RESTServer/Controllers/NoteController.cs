e279993cbd5f05732d8a04a8a9c5ce1f96a08d5d
Ex0oleg <olegexo@gmail.com>
2023-06-06 00:06:01 +0300

    added controllers for notes and users

---
+++ RESTServer/Controllers/NoteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+    }
+}
