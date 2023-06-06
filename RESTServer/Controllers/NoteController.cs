1397c4931e23a9f181fe3f5ef712e057f8968c27
Ex0oleg <olegexo@gmail.com>
2023-06-06 20:57:49 +0300

    populated the controllers

--- RESTServer/Controllers/NoteController.cs
+++ RESTServer/Controllers/NoteController.cs
@@ -1,11 +1,44 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using RESTServer.DatabaseOperators;
+using RESTServer.Models;
 
 namespace RESTServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class NoteController : ControllerBase
     {
+        NoteOperator noteOperator = new NoteOperator();
+
+        [HttpGet(Name = "ByUserID")]
+        public async Task<IEnumerable<Note>> Get(int userID)
+        {
+            Task<IEnumerable<Note>> InoteTask = noteOperator.GetNotesByUserID(userID);
+            IEnumerable<Note> Inote = await InoteTask;
+            return Inote;
+        }
+        [HttpPut(Name = "InsertNote")]
+        public void Put(string Title, string Time, int UserID, DateTime NoteDate, bool IsChecked)
+        {
+            Note note = new Note(Title, Time, UserID, NoteDate, IsChecked);
+            noteOperator.InsertNote(note);
+        }
+        [HttpDelete(Name = "DeleteNote")]
+        public void Delete(int ID)
+        {
+            noteOperator.DeleteNoteByID(ID);
+        }
+        [HttpPatch(Name = "UpdateNote")]
+        public void Patch(int ID, string Title, string Time, bool isChecked)
+        {
+            Note note = new Note();
+            note.Title = Title;
+            note.Time = Time;
+            note.IsChecked = isChecked;
+            note.ID = ID;
+            noteOperator.UpdateNote(note);
+        }
     }
 }
--- RESTServer/Controllers/UserController.cs
+++ RESTServer/Controllers/UserController.cs
@@ -1,11 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RESTServer.DatabaseOperators;
+using RESTServer.Models;
 
 namespace RESTServer.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UserController : ControllerBase
     {
+        UserOperator userOperator = new UserOperator();
+        [HttpGet(Name = "ByLogin")]
+        public async Task<IEnumerable<User>> Get(string login)
+        {
+            Task<IEnumerable<User>> IuserTask = userOperator.GetUserByLogin(login);
+            IEnumerable<User> Iuser = await IuserTask;
+            return Iuser;
+        }
+        [HttpPut(Name = "InsertUser")]
+        public void Put(string FirstName, string LastName, string Login, string Password, string Email)
+        {
+            User user = new User(FirstName, LastName, Login, Password, Email);
+            userOperator.InsertUser(user);
+        }
     }
 }
