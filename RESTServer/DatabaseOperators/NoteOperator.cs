523b8015e44c091d24e349067025780aaf24683c
Ex0oleg <olegexo@gmail.com>
2023-06-06 20:59:19 +0300

    added abstraction for data access for each model

---
+++ RESTServer/DatabaseOperators/NoteOperator.cs
@@ -0,0 +1,32 @@
+﻿using RESTServer.DataUtils;
+using RESTServer.Models;
+
+namespace RESTServer.DatabaseOperators
+{
+    public class NoteOperator
+    {
+        private DataAccess dataAccess = new DataAccess();
+        public async void DeleteNoteByID(int id)
+        {
+            Note note = new Note();
+            note.ID = id;
+            await dataAccess.SaveData("dbo.spDeleteNoteByID", new { note.ID });
+        }
+        public async Task<IEnumerable<Note>> GetNotesByUserID(int id)
+        {
+            User user = new User();
+            user.ID = id;
+            Task<IEnumerable<Note>> TaskNotes = dataAccess.LoadData<Note, dynamic>("dbo.spGetNotesByUserID", new { user.ID });
+            IEnumerable<Note> INotes = await TaskNotes;
+            return INotes;
+        }
+        public async void InsertNote(Note note)
+        {
+            await dataAccess.SaveData("dbo.spInsertNote", new { note.Title, note.Time, note.UserID, note.NoteDate, note.IsChecked });
+        }
+        public async void UpdateNote(Note note)
+        {
+            await dataAccess.SaveData("dbo.spUpdateNoteByID", new { note.ID, note.Title, note.Time, note.IsChecked });
+        }
+    }
+}
---
+++ RESTServer/DatabaseOperators/UserOperator.cs
@@ -0,0 +1,22 @@
+﻿using RESTServer.DataUtils;
+using RESTServer.Models;
+
+namespace RESTServer.DatabaseOperators
+{
+    public class UserOperator
+    {
+        private DataAccess dataAccess = new DataAccess();
+        public async Task<IEnumerable<User>> GetUserByLogin(string login)
+        {
+            User user = new User();
+            user.Login = login;
+            Task<IEnumerable<User>> TaskUser = dataAccess.LoadData<User, dynamic>("dbo.spGetUserByLogin", new { user.Login });
+            IEnumerable<User> IUser = await TaskUser;
+            return IUser;
+        }
+        public async void InsertUser(User user)
+        {
+            await dataAccess.SaveData("dbo.spInsertUser", new { user.FirstName, user.LastName, user.Login, user.Password, user.Email });
+        }
+    }
+}
