b7ec346a82647ae771180c3e9d4d8c1eae44334f
Ex0oleg <olegexo@gmail.com>
2023-06-10 18:14:24 +0300

    Wired login to login window

---
+++ WPF_FrontEnd/AppVars/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using RESTServer.Models;
+using System.Collections.Generic;
+using System.Windows.Documents;
+
+namespace WPF_FrontEnd.AppVars
+{
+    public class GlobalVariables
+    {
+        public static User CurrentUser { get; set; }
+
+        public static List<Note> CurrentNotes { get; set; }
+    }
+}
--- WPF_FrontEnd/Windows/LoginWindow.xaml.cs
+++ WPF_FrontEnd/Windows/LoginWindow.xaml.cs
@@ -14,7 +14,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_FrontEnd.AppVars;
 using WPF_FrontEnd.RESTUtils;
 
 namespace WPF_FrontEnd
@@ -24,6 +26,7 @@ namespace WPF_FrontEnd
     /// </summary>
     public partial class LoginWindow : Window
     {
+        RESTClient WebClient = new RESTClient();
         public LoginWindow()
         {
             InitializeComponent();
@@ -38,9 +41,41 @@ namespace WPF_FrontEnd
             Close();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void RegButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            Hide();
+            registrationWindow.ShowDialog();
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(LoginTextBox.Text) && !string.IsNullOrEmpty(PasswordTextBox.Password))
+            {
+                if (WebClient.UserExists(LoginTextBox.Text))
+                {
+                    var tempuser = WebClient.GetUserByLogin(LoginTextBox.Text);
+                    if (tempuser.Password == PasswordTextBox.Password)
+                    {
+                        GlobalVariables.CurrentUser = tempuser;
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.ShowDialog();
+                        PasswordTextBox.Password = "";
+                        LoginTextBox.Text = "";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password is incorrect");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("User with that login doesn't exist");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Login or password empty");
+            }
         }
     }
 }
