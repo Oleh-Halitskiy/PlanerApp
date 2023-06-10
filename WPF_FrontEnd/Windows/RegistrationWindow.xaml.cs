11b47ba19263939163257da7bef1cdceea892c6b
Ex0oleg <olegexo@gmail.com>
2023-06-10 19:44:16 +0300

    completed registration logic

--- WPF_FrontEnd/Windows/RegistrationWindow.xaml.cs
+++ WPF_FrontEnd/Windows/RegistrationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RESTServer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPF_FrontEnd.RESTUtils;
 
 namespace WPF_FrontEnd
 {
@@ -19,6 +21,7 @@ namespace WPF_FrontEnd
     /// </summary>
     public partial class RegistrationWindow : Window
     {
+        RESTClient WebClient = new RESTClient();
         public RegistrationWindow()
         {
             InitializeComponent();
@@ -31,7 +34,28 @@ namespace WPF_FrontEnd
 
         private void RegButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if(!string.IsNullOrEmpty(FNTextBox.Text) &&
+               !string.IsNullOrEmpty(LNTextBox.Text) && 
+               !string.IsNullOrEmpty(LoginTextBox.Text) &&
+               !string.IsNullOrEmpty(PasswordBox.Password) && 
+               !string.IsNullOrEmpty(EmailTextBox.Text))
+            {
+                if(!WebClient.UserExists(LoginTextBox.Text))
+                {
+                    User user = new User(FNTextBox.Text,LNTextBox.Text,LoginTextBox.Text,PasswordBox.Password,EmailTextBox.Text);
+                    WebClient.InsertPerson(user);
+                    MessageBox.Show("Registration is successful");
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("User with such login already exist");
+                }
+            }
+            else
+            {
+                MessageBox.Show("One of the fields is empty check your input");
+            }
         }
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
